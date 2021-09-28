c5ca30712aea81296a437c846330c51605a0711f
Francois Pasquier <git@pasquierfrancois.com>
2021-09-28 10:00:42 +0200

    nvim: update nvim-tree config

--- .config/nvim/lua/plugins/config/nvim-tree/init.lua
+++ .config/nvim/lua/plugins/config/nvim-tree/init.lua
@@ -1,6 +1,7 @@
 vim.g.nvim_tree_ignore = {'.git', 'node_modules'}
 vim.g.nvim_tree_gitignore = 1
-vim.g.nvim_tree_auto_close = 1
-vim.g.nvim_tree_update_cwd = 1
-vim.g.nvim_tree_disable_netrw = 1
--- vim.g.nvim_tree_auto_open = 1
+
+require'nvim-tree'.setup {
+  disable_netrw       = true,
+  update_cwd          = true,
+}
