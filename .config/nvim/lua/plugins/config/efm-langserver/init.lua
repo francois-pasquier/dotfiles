f2114b1e6c2a2491a3be762671af636b9f4f5356
Francois Pasquier <git@pasquierfrancois.com>
2021-10-12 15:05:13 +0200

    nvim: add stylelint + eslint lint support

--- .config/nvim/lua/plugins/config/efm-langserver/init.lua
+++ .config/nvim/lua/plugins/config/efm-langserver/init.lua
@@ -1,6 +1,8 @@
 local root_markers = { ".git/" }
 
 local eslint = {
+  formatCommand = "eslint_d --stdin --fix-to-stdout --stdin-filename=${INPUT}",
+  formatStdin = true,
   lintCommand = "eslint_d -f unix --stdin --stdin-filename ${INPUT}",
   lintFormats = {"%f:%l:%c: %m"},
   lintIgnoreExitCode = true,
@@ -30,4 +32,14 @@ require "lspconfig".efm.setup {
         languages = languages
     },
     filetypes = vim.tbl_keys(languages)
+}
+
+require "lspconfig".stylelint_lsp.setup {
+  settings = {
+    stylelintplus = {
+       autoFixOnSave = true,
+       autoFixOnFormat = true
+    },
+    filetypes = { "css", "less", "postcss", "scss", "sugarss", "vue", "wxss", "js", "jsx", "ts", "tsx" }
+  }
 }
