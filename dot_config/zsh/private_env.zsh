e7f2aea413ea05709e3c22111d5c7bb04fe96158
Francois Pasquier <git@pasquierfrancois.com>
2025-06-02 13:15:19 +0200

    chore: addc chruby and ruby-install

--- dot_config/zsh/private_env.zsh
+++ dot_config/zsh/private_env.zsh
@@ -3,3 +3,10 @@ export CPPFLAGS="-I/opt/homebrew/opt/openjdk@11/include"
 export LESS='-R'
 
 eval "$(fnm env --use-on-cd)"
+
+source /opt/homebrew/opt/chruby/share/chruby/chruby.sh
+
+source /opt/homebrew/opt/chruby/share/chruby/auto.sh
+
+chruby ruby-3.4.4
+
